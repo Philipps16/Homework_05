b5b49dd82d6a19c5a5b5637d8cce8494473aafe7
Philipp <press.fm@mail.ru>
2022-12-17 13:16:56 +0300

    Выполнил упражнение 03

---
+++ Example_03_razniza_min_i_max/Program.cs
@@ -0,0 +1,41 @@
+﻿void InputArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = Math.Round ((new Random().NextDouble () * 100), 2);
+    }
+}
+
+double ReleaseArray (double [] array)
+{
+    int i = 0;
+    double max = array[i];
+    double min = array[i];
+
+
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array [i] > max)
+        max = array [i];
+    }
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        min = array [i];
+    }
+    double def = max - min;
+    return def;
+
+}
+
+
+
+
+Console.Clear();
+Console.WriteLine ("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [] array = new double [n];
+
+InputArray(array);
+Console.WriteLine ($"[{string.Join(", ", array)}]");
+Console.Write ($"Разница между максимальным и минимальным числом равна {ReleaseArray (array)}");
